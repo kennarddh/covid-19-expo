1c79afa3b825277ab169332fc0d426b764715788
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-29 13:21:53 +0700

    feat: Fetch covid 19 data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import React, { useState } from 'react'
+
 import { registerRootComponent } from 'expo'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native' // eslint-disable-line import/namespace
+import { Text, TouchableOpacity } from 'react-native' // eslint-disable-line import/namespace
+
+import { Container } from './Styles'
 
 const App = () => {
+	const [Covid19Data, SetCovid19Data] = useState({})
+
+	const FetchCovid19Data = () => {
+		fetch('https://covid19.mathdro.id/api')
+			.then(response => response.json())
+			.then(data => {
+				SetCovid19Data({
+					confirmed: data.confirmed.value,
+					recovered: data.recovered.value,
+					deaths: data.deaths.value,
+				})
+			})
+	}
+
+	const OnPress = () => {
+		FetchCovid19Data()
+	}
+
 	return (
-		<View>
+		<Container>
 			<Text>Open up App.js to start working on your app!</Text>
+			<TouchableOpacity onPress={OnPress}>
+				<Text>Press Here</Text>
+			</TouchableOpacity>
 			<StatusBar style='auto' />
-		</View>
+		</Container>
 	)
 }
 
