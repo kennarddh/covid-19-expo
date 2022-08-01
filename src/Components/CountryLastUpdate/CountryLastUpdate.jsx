f5d2c2c4a202345dd6c661383630b7e4f4d6cebf
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-01 19:40:55 +0700

    feat: Container

--- src/Components/CountryLastUpdate/CountryLastUpdate.jsx
+++ src/Components/CountryLastUpdate/CountryLastUpdate.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
+
 import DataTable, { COL_TYPES } from 'react-native-datatable-component'
 
 import { Countries } from '../../Utils/Api'
 import FormatCountriesNewUpdateData from '../../Utils/FormatCountriesNewUpdateData'
+
+import { Container } from './Styles'
 
 const CountryLastUpdate = () => {
 	const [Data, SetData] = useState([])
@@ -20,28 +23,42 @@ const CountryLastUpdate = () => {
 	}, [])
 
 	return (
-		<DataTable
-			data={Data}
-			colNames={[
-				'displayName',
-				'population',
-				'lastUpdateAt',
-				'totalDeaths',
-			]}
-			colSettings={[
-				{ name: 'displayName', type: COL_TYPES.STRING, width: '20%' },
-				{ name: 'population', type: COL_TYPES.INT, width: '20%' },
-				{
-					name: 'lastUpdateAt',
-					type: COL_TYPES.STRING,
-					width: '30%',
-				},
-				{ name: 'totalDeaths', type: COL_TYPES.INT, width: '20%' },
-			]}
-			noOfPages={1}
-			backgroundColor='#d3d3d3' // Table Background Color
-			headerLabelStyle={{ color: 'grey', fontSize: 12 }} // Text Style Works
-		/>
+		<Container>
+			<DataTable
+				data={Data}
+				colNames={[
+					'displayName',
+					'population',
+					'lastUpdateAt',
+					'totalDeaths',
+				]}
+				colSettings={[
+					{
+						name: 'displayName',
+						type: COL_TYPES.STRING,
+						width: '20%',
+					},
+					{
+						name: 'population',
+						type: COL_TYPES.INT,
+						width: '20%',
+					},
+					{
+						name: 'lastUpdateAt',
+						type: COL_TYPES.STRING,
+						width: '30%',
+					},
+					{
+						name: 'totalDeaths',
+						type: COL_TYPES.INT,
+						width: '20%',
+					},
+				]}
+				noOfPages={Data.length / 10}
+				backgroundColor='#d3d3d3' // Table Background Color
+				headerLabelStyle={{ color: 'grey', fontSize: 12 }} // Text Style Works
+			/>
+		</Container>
 	)
 }
 
--- src/Components/CountryLastUpdate/Styles.jsx
+++ src/Components/CountryLastUpdate/Styles.jsx
@@ -1,1 +1,9 @@
 import styled from 'styled-components/native'
+
+export const Container = styled.View`
+	border-radius: 15px;
+	width: 90%;
+	background-color: #d3d3d3;
+	padding: 10px;
+	box-sizing: border-box;
+`
