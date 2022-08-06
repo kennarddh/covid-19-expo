7ff65d5a2fe2123ba05a831ba46414d2b618e70f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-06 13:31:44 +0700

    fix: Sync items with data props

--- src/Components/Select/Select.jsx
+++ src/Components/Select/Select.jsx
@@ -5,15 +5,17 @@ import DropDownPicker from 'react-native-dropdown-picker'
 const Select = ({ data, onSelect, disabled = false, defaultButtonText }) => {
 	const [IsOpen, SetIsOpen] = useState(false)
 	const [Value, SetValue] = useState(null)
-	const [Items, SetItems] = useState(
-		data.map(value => ({ value, label: value }))
-	)
+	const [Items, SetItems] = useState([])
 
 	useEffect(() => {
 		if (!Value) return
 
 		onSelect(Value)
 	}, [Value, onSelect])
+
+	useEffect(() => {
+		SetItems(data.map(value => ({ value, label: value })))
+	}, [data])
 
 	return (
 		<DropDownPicker
